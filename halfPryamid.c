7d878e26275c6a7322db090ae1342d3c7b9af861
saravnansoftdev <saravanan.softdev@gmail.com>
2024-09-20 05:51:30 +0530

    .

--- halfPryamid.c
+++ halfPryamid.c
@@ -2,5 +2,14 @@
 
 int main()
 {
+    int i,j,n=5;
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
     return;
 }
